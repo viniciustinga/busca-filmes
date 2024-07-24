532f41dc7261dd39c2187c507bc1abce2deb9826
viniciustinga <viniciustinga@gmail.com>
2024-07-24 16:29:14 -0300

    Adding HashRouter nito main

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import { Home, Movie, Search } from "./pages/index.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<App />}>
-          <Route path="/busca-filmes" element={<Home />} />
-          <Route path="/busca-filmes/movie/:id" element={<Movie />} />
-          <Route path="/busca-filmes/search" element={<Search />} />
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="search" element={<Search />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
